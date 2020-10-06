3ecd3045cc4154a7b336a707f5e100a535df26dd
Vatsal <rdbcasillas11@gmail.com>
2020-10-06 14:57:27 -0500

    header highlights the active module

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,18 +6,18 @@
             </b-navbar-brand>
             <b-navbar-brand tag="h1"> <b-link href="/"><b>AxonRegDb</b></b-link> </b-navbar-brand>
             <b-navbar-nav class="mx-auto ml-auto">
-                <b-nav-item @click="checkroute" v-for="link in links" 
+                <b-nav-item class="current" @click="checkroute" v-for="link in links" 
                 :to="{name: link.route, 
                 params: {plotType: link.route}}"
                 :key="link.name"
                 :active="link.state">
                     {{ link.name }}
                 </b-nav-item>
-            <b-nav-item-dropdown text="TF-Bind" left>
+            <b-dropdown :variant="tfstate ? 'outline-success' : 'outline-secondary'" @click="dropdownClick" text="TF-Bind" left>
                 <b-dropdown-item :to="{ name: 'selectenhanceranalysis'}">Enhancer</b-dropdown-item>
                 <b-dropdown-divider></b-dropdown-divider>
                 <b-dropdown-item :to="{ name: 'promotertf'}">Promoter</b-dropdown-item>
-            </b-nav-item-dropdown>
+            </b-dropdown>
             </b-navbar-nav>
                 <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-url="https://axonregdb.netlify.com/" data-hashtags="#axonregdb #spinalcordinjury" data-show-count="false">Tweet</a>
                 <a href="https://github.com/rdbcasillas/axonregDB"> <b-img class="githublogo" src="./Github-Mark.png"></b-img> </a>
@@ -28,6 +28,7 @@
 
 <script>
     import * as _ from "lodash";
+    import * as $  from "jquery";
     export default {
         name: "Header",
         data: function() {
@@ -59,7 +60,8 @@
                         route: 'histone',
                         state: false
                     }
-                ]
+                ],
+                tfstate: false
 
             }
         },
@@ -71,6 +73,24 @@
                 })
                 let currlink = _.find(this.links,{route: this.$route.name})
                 currlink.state = true;
+            },
+            dropdownClick() {
+                console.log("hi")
+            }
+        },
+        watch: {
+            $route: function(){
+               this.tfstate = false;
+               let currlink = _.find(this.links,{route: this.$route.name})
+               if (currlink) {
+                currlink.state = true;
+               }
+               else {
+                this.tfstate = true;
+                this.links.forEach((link)=>{
+                    link.state = false
+                })
+               }
             }
         }
     }
@@ -92,5 +112,10 @@ li.nav-item {
 }
 .githublogo {
     margin-left: 10px;
+}
+.nav-link.active
+{
+    color: #28a745 !important;
+    font-weight: bold;
 }
 </style>
